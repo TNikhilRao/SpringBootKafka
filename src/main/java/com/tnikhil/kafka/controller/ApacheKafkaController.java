e052aa787bbbc4d90b3d625aa388f38c58106293
Nikhil Tirmanwar <Nikhil.Tirmanwar@synechron.com>
2018-07-23 18:05:44 +0530

    Added Main Code

---
+++ src/main/java/com/tnikhil/kafka/controller/ApacheKafkaController.java
@@ -0,0 +1,26 @@
+package com.tnikhil.kafka.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.tnikhil.kafka.producer.Sender;
+
+/**
+ * @author Nikhil.Tirmanwar
+ *
+ */
+@RestController("/kafka/")
+public class ApacheKafkaController {
+
+	@Autowired
+	Sender sender;
+
+	@GetMapping("{message}")
+	public void testMe(@PathVariable("message") String message) {
+
+		sender.send(message);
+
+	}
+}
